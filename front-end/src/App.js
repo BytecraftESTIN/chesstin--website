c63dceb00cab123b8a8837436b65bd63ddda1a52
MehidGN <mahdign12@gmail.com>
2022-12-06 02:38:54 +0100

    first

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import { Container } from "@mantine/core"
+function App() {
+  return (
+    <>
+      <Container>
+        <p>hello hi</p>
+      </Container>
+    </>
+  );
+}
+
+export default App;
